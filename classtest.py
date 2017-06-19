d28fdb0640e0ecc762ec36ba9d2a9a08553da97e
wangzheng62 <363579018@qq.com>
2017-06-19 22:21:36 +0800

    zz

---
+++ classtest.py
@@ -0,0 +1,28 @@
+﻿import mysql.connector
+
+class Mysqlservermetaclass(type):
+	def __new__(cls, name, bases, attrs):
+		attrs['server']=name
+		print('cls:%s,name:%s,bases:%s,attrs:%s'%(cls, name, bases, attrs))
+		return type.__new__(cls, name, bases, attrs)
+class MysqlserverBase(metaclass=Mysqlservermetaclass):
+    pass
+
+# 数据库
+class MysqlDbmetaclass(Mysqlservermetaclass):
+	def __new__(cls, name, bases, attrs):
+		attrs['db']= name
+		print('cls:%s,name:%s,bases:%s,attrs:%s'%(cls, name, bases, attrs))
+		return type.__new__(cls, name, bases, attrs)
+class MysqlDbBase(metaclass=MysqlDbmetaclass):
+    pass
+	
+class Localhost(MysqlserverBase):
+	pass
+class Test(MysqlDbBase,Localhost):
+	pass
+	
+if __name__=='__main__':
+	print(Localhost.server)
+	print(Test.server)
+	print(Test.db)
