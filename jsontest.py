647a88202f5d59674a1ca73cc390c5e3d60a55ea
wangzheng62 <363579018@qq.com>
2017-06-21 17:18:19 +0800

    1

--- jsontest.py
+++ jsontest.py
@@ -1,28 +1,32 @@
 import json
 
 option = {
-            tooltip: {
-                show: true
+            'tooltip': {
+                'show': True
             },
-            legend: {
-                data:['销量']
+            'legend': {
+                'data':['销量']
             },
-            xAxis : [
+            'xAxis' : [
                 {
-                    type : 'time',
-                    data : ["2015","2016","2017","2018","2019","2020"]
+                    'type' : 'time',
+                    'data' : ["2015","2016","2017","2018","2019","2020"]
                 }
             ],
-            yAxis : [
+            'yAxis' : [
                 {
-                    type : 'value'
+                    'type' : 'value'
                 }
             ],
-            series : [
+            'series' : [
                 {
                     "name":"销量",
                     "type":"line",
                     "data":[5, 20, 40, 10, 10, 20]
                 }
             ]
-        };
+        }
+if __name__=='__main__':
+	print(option)
+	s=json.dumps(option)
+	print(s)
